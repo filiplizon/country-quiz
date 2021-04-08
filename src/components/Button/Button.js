07424d8cf515481d90e58751cdb8aa212d3614f6
Filip Lizoń <filip.lizon1@gmail.com>
2021-04-08 19:33:42 +0200

    create Button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = ({ children }) => <StyledButton>{children}</StyledButton>;
+
+const StyledButton = styled.button`
+  width: 100%;
+  background-color: ${({ theme }) => theme.primary};
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 10px 0;
+  text-align: center;
+  transition: background-color 0.2s ease;
+  cursor: pointer;
+  outline-color: ${({ theme }) => theme.secondary};
+
+  :hover {
+    background-color: ${({ theme }) => theme.secondary};
+  }
+
+  @media (min-width: 1200px) {
+    font-size: ${({ theme }) => theme.fontSize.m};
+  }
+`;
+
+export default Button;
