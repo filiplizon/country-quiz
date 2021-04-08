2acf1bd5c36a4f20a951f981d86896d33e9ce726
Filip Lizoń <filip.lizon1@gmail.com>
2021-04-08 19:12:25 +0200

    add global variables

---
+++ src/theme/mainTheme.js
@@ -0,0 +1,16 @@
+export const theme = {
+  primary: 'hsl(204, 57%, 34%)',
+  secondary: 'hsl(204, 57%, 49%)',
+  grey100: 'hsl(0, 0%, 96%)',
+  grey200: 'hsl(0, 0%, 94%)',
+  black: 'hsl(0, 0%, 0%)',
+  fontWeight: { light: 300, bold: 600 },
+  fontSize: {
+    xs: '1rem',
+    s: '1.6rem',
+    m: '2rem',
+    l: '2.5rem',
+    xl: '3rem',
+    xxl: '3.5rem',
+  },
+};
