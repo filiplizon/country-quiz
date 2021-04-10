6f03716af68e91529cbab6a4327c3ec79a5da5c2
Filip Lizoń <filip.lizon1@gmail.com>
2021-04-10 20:33:27 +0200

    create flag component

---
+++ src/components/Flag/Flag.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+  width: 140px;
+  height: 100px;
+  background-color: white;
+`;
+
+const StyledFlag = styled.img`
+  width: 100%;
+  height: 100%;
+  border: 1px solid ${({ theme }) => theme.grey100};
+`;
+
+const Flag = ({ flag, name }) => (
+  <StyledWrapper>
+    <StyledFlag src={flag} alt={name} />
+  </StyledWrapper>
+);
+
+export default Flag;
