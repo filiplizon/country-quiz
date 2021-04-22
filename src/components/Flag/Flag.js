93a1baf0433c37dbb5839f0ee2afde96ad7c756a
Filip Lizoń <filip.lizon1@gmail.com>
2021-04-22 23:23:08 +0200

    add name of country while quiz

--- src/components/Flag/Flag.js
+++ src/components/Flag/Flag.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import Paragraph from 'components/Paragraph/Paragraph';
 
 const StyledWrapper = styled.div`
   width: 140px;
   height: 100px;
   background-color: white;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
@@ -15,9 +17,37 @@ const StyledFlag = styled.img`
   border: 1px solid ${({ theme }) => theme.grey200};
 `;
 
-const Flag = ({ flag, name }) => (
+const StyledParagraph = styled(Paragraph)`
+  text-align: center;
+
+  @media (min-width: 360px) {
+    font-size: ${({ theme }) => theme.fontSize.m};
+  }
+
+  @media (min-width: 500px) and (orientation: landscape) {
+    font-size: ${({ theme }) => theme.fontSize.s};
+  }
+
+  @media (min-width: 768px) {
+    font-size: ${({ theme }) => theme.fontSize.xl};
+  }
+
+  @media (min-width: 800px) and (orientation: landscape) {
+    font-size: ${({ theme }) => theme.fontSize.m};
+  }
+
+  @media (min-width: 1024px) and (orientation: landscape) {
+    font-size: ${({ theme }) => theme.fontSize.l};
+  }
+  @media (min-width: 1200px) {
+    font-size: ${({ theme }) => theme.fontSize.m};
+  }
+`;
+
+const Flag = ({ flag, name, quiz }) => (
   <StyledWrapper>
     <StyledFlag src={flag} alt={name} />
+    {quiz && <StyledParagraph>{name}</StyledParagraph>}
   </StyledWrapper>
 );
 
