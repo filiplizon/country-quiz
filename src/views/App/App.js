96bb6e5d37d31c93f8a814d500ddfa32ce09f781
Filip Lizoń <filip.lizon1@gmail.com>
2021-04-27 00:45:40 +0200

    add store provider

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from 'store';
 import MainTemplate from 'templates/MainTemplate';
-
 import Quiz from 'components/Quiz/Quiz';
 import HomePage from 'components/HomePage/HomePage';
 
 const App = () => (
-  <BrowserRouter>
-    <MainTemplate>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/quiz" component={Quiz} />
-      </Switch>
-    </MainTemplate>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <MainTemplate>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/quiz" component={Quiz} />
+        </Switch>
+      </MainTemplate>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
