fd72aa1d814efd5e3025121795789d79fe33a96f
Filip Lizoń <filip.lizon1@gmail.com>
2021-04-30 10:22:06 +0200

    move setCountriesLevel into SelectionPanel

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,34 +1,23 @@
-import React, { useEffect } from 'react';
-import SelectionPanel from 'components/SelectionPanel/SelectionPanel';
+import React from 'react';
 import { connect } from 'react-redux';
-import actions from 'actions/actions';
-import QuestionWrapper from 'components/QuestionWrapper/QuestionWrapper';
 import QuizTemplate from 'templates/QuizTemplate';
+import SelectionPanel from 'components/SelectionPanel/SelectionPanel';
+import QuestionWrapper from 'components/QuestionWrapper/QuestionWrapper';
 import Summary from 'components/Summary/Summary';
 
-const Quiz = ({ setCountriesLevel, level, counter, isChecked }) => {
-  useEffect(() => {
-    setCountriesLevel();
-  }, []);
-
-  return (
-    <QuizTemplate>
-      {counter === 9 && isChecked ? (
-        <Summary />
-      ) : (
-        <> {level ? <QuestionWrapper /> : <SelectionPanel />}</>
-      )}
-    </QuizTemplate>
-  );
-};
-
-const mapDispatchToProps = (dispatch) => ({
-  setCountriesLevel: () => dispatch(actions.setCountriesLevel()),
-});
+const Quiz = ({ level, counter, isChecked }) => (
+  <QuizTemplate>
+    {counter === 9 && isChecked ? (
+      <Summary />
+    ) : (
+      <>{level ? <QuestionWrapper /> : <SelectionPanel />}</>
+    )}
+  </QuizTemplate>
+);
 
 const mapStateToProps = (state) => {
   const { level, counter, isChecked } = state;
   return { level, counter, isChecked };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Quiz);
+export default connect(mapStateToProps)(Quiz);
