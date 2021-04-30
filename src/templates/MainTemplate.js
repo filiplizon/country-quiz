adcede4f7cdd218867583b1293b46d70b0cc8458
Filip Lizoń <filip.lizon1@gmail.com>
2021-04-30 10:14:32 +0200

    fetch countries in main template

--- src/templates/MainTemplate.js
+++ src/templates/MainTemplate.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import PropTypes from 'prop-types';
 import GlobalStyle from 'helpers/GlobalStyle';
 import { theme } from 'helpers/mainTheme';
+import { connect } from 'react-redux';
+import { getAllCountries as getAllCountriesAction } from 'operations/operations';
 
-const MainTemplate = ({ children }) => (
-  <div>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
-  </div>
-);
+const MainTemplate = ({ children, getAllCountries }) => {
+  useEffect(() => {
+    getAllCountries();
+  }, []);
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </>
+  );
+};
 
 MainTemplate.propTypes = {
   children: PropTypes.element.isRequired,
 };
 
-export default MainTemplate;
+const mapDispatchToProps = (dispatch) => ({
+  getAllCountries: () => dispatch(getAllCountriesAction()),
+});
+
+export default connect(null, mapDispatchToProps)(MainTemplate);
